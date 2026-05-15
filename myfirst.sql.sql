f758dc6bf0be2b8fb1bbc9a9e71adc5f96dc41e3
DoyleRyoo <ryodoil1812@gmail.com>
2026-05-15 11:42:21 +0900

    oracle practice first commit

---
+++ myfirst.sql.sql
@@ -0,0 +1,14 @@
+-- ctrl + Enter : 실행
+/*
+    여러라인 주식
+*/
+SELECT * FROM tab;
+
+/*
+ 컬럼명, 테이블에 별칭(alias)을 지정할 수 있다.
+별칭(alias)에 공백이 있을때는 " "(따옴표)를 지정한다.
+*/
+
+SELECT salary, salary*10 AS bonus
+
+FROM employees;
